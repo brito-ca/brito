3e809b392ca53ce0db498981f154dcf07d4b352a
marceloglacial <glacial@gmail.com>
2023-07-26 12:41:55 +0000

    Change from Nuxt to Next and update initial pages

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Logo } from './components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Logo />
+        <Text>Comming soon!</Text>
+      </View>
+      <StatusBar />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  content: {
+    alignItems: 'center',
+    gap: 20,
+    marginBottom: 100,
   },
 });
---
+++ apps/website/app/page.tsx
@@ -0,0 +1,10 @@
+import { Logo } from '@/components';
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center'>
+      <Logo />
+      <p className='p-4 text-red-600'>Comming soon!</p>
+    </main>
+  );
+}
